a858b1ecc05ca96066c6d46537f02c99048a0127
mumumu3560 <puzmgamgam121@gmail.com>
2024-06-30 03:08:22 +0900

    DBのriverpod

---
+++ lib/riverpod/db_admin/db_admin.dart
@@ -0,0 +1,33 @@
+
+import 'package:trade_stats_accumulation/database/drift/database_1/database.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'db_admin.g.dart';
+
+@riverpod
+class DbAdminNotifier extends _$DbAdminNotifier{
+
+  @override
+  MyDatabase build() {
+    String db = "trade_stats_accumulation_database";
+    MyDatabase myDB = MyDatabase(dbName: db);
+    return myDB;
+  }
+
+  //ここでdb変更。
+  //classifier_database、classifier_database_1、classifier_database_2のいずれかを引数に取る。
+  Future<void> updateDB(String db) async{
+    //myDatabaseをいったん破棄して、新しいdbを作成する。
+    state = MyDatabase(dbName: db);
+  }
+
+  Future<void> closeDB() async{
+    await state.close();
+  }
+
+  
+
+
+
+
+}
---
+++ lib/riverpod/db_admin/db_admin.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'db_admin.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dbAdminNotifierHash() => r'a185c2d492322dea065785ce44ab007a417f21d8';
+
+/// See also [DbAdminNotifier].
+@ProviderFor(DbAdminNotifier)
+final dbAdminNotifierProvider =
+    AutoDisposeNotifierProvider<DbAdminNotifier, MyDatabase>.internal(
+  DbAdminNotifier.new,
+  name: r'dbAdminNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$dbAdminNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$DbAdminNotifier = AutoDisposeNotifier<MyDatabase>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
