e51e260db68f17475cedb71823b6964d81e75279
mumumu3560 <puzmgamgam121@gmail.com>
2024-06-30 04:07:45 +0900

    テーマをriverpodで管理しておく

---
+++ lib/riverpod/theme/theme.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'theme.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$themeModeNotifierHash() => r'15dd7fd9e108e1b73f6158ff9a0c93694395a87e';
+
+/// See also [ThemeModeNotifier].
+@ProviderFor(ThemeModeNotifier)
+final themeModeNotifierProvider =
+    AutoDisposeAsyncNotifierProvider<ThemeModeNotifier, int>.internal(
+  ThemeModeNotifier.new,
+  name: r'themeModeNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$themeModeNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ThemeModeNotifier = AutoDisposeAsyncNotifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
