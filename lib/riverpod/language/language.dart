bff39c980775c5dd59da3e5c401dc9fe7cbd9fcb
mumumu3560 <puzmgamgam121@gmail.com>
2024-06-30 03:08:46 +0900

    ユーザーがどの言語を使うかについてのriverpod

---
+++ lib/riverpod/language/language.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:trade_stats_accumulation/language/i18n/strings.g.dart';
+
+part 'language.g.dart';
+//ここで言語を指定する。
+@riverpod
+class LanguageNotifier extends _$LanguageNotifier{
+
+  static final languages = [
+    "en",
+    "ja",
+    "pt",
+    "es",
+  ];
+
+  @override
+  AppLocale build() {
+    if(!languages.contains(LocaleSettings.currentLocale.languageCode)){
+      return AppLocale.en;
+    }
+    return LocaleSettings.currentLocale;
+
+  }
+
+  //ここでthemeModeを変更する。
+  void updateLocale(AppLocale newLocale) {
+
+    state = newLocale;
+    LocaleSettings.setLocale(newLocale);
+  }
+
+
+}
---
+++ lib/riverpod/language/language.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'language.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$languageNotifierHash() => r'79f2d2943e1ce395517f0c418bbee1d026e992cd';
+
+/// See also [LanguageNotifier].
+@ProviderFor(LanguageNotifier)
+final languageNotifierProvider =
+    AutoDisposeNotifierProvider<LanguageNotifier, AppLocale>.internal(
+  LanguageNotifier.new,
+  name: r'languageNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$languageNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$LanguageNotifier = AutoDisposeNotifier<AppLocale>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
