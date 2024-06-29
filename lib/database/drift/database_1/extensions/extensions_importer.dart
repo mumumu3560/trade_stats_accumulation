0c79ca06038865265d3466d4254e29c00c3eae43
mumumu3560 <puzmgamgam121@gmail.com>
2024-06-30 04:07:02 +0900

    extensionsの関数をまとめてimportできるようにする

---
+++ lib/database/drift/database_1/extensions/extensions_importer.dart
@@ -0,0 +1,5 @@
+export 'package:trade_stats_accumulation/database/drift/database_1/extensions/setting/settings_operations.dart';
+export 'package:trade_stats_accumulation/database/drift/database_1/extensions/tag_attribute/tag_attributes_operations.dart';
+export 'package:trade_stats_accumulation/database/drift/database_1/extensions/trade_data/trade_datas_operations.dart';
+export 'package:trade_stats_accumulation/database/drift/database_1/extensions/tagged_trade_data/tagged_trade_datas_operations.dart';
+export 'package:trade_stats_accumulation/database/drift/database_1/extensions/tag/tags_operations.dart';
