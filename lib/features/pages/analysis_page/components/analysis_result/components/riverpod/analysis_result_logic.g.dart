8772e6e63090115aff0a529d2fcd18735779e743
mumumu3560 <puzmgamgam121@gmail.com>
2024-07-25 19:02:09 +0900

    ここでfilterを実行しその結果を管理する。

---
+++ lib/features/pages/analysis_page/components/analysis_result/components/riverpod/analysis_result_logic.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'analysis_result_logic.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$analysisResultNotifierHash() =>
+    r'19de47ae4f8f5ae52c23ba156b47094d02094f52';
+
+/// See also [AnalysisResultNotifier].
+@ProviderFor(AnalysisResultNotifier)
+final analysisResultNotifierProvider = AutoDisposeNotifierProvider<
+    AnalysisResultNotifier, AnalysisResultState>.internal(
+  AnalysisResultNotifier.new,
+  name: r'analysisResultNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$analysisResultNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AnalysisResultNotifier = AutoDisposeNotifier<AnalysisResultState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
