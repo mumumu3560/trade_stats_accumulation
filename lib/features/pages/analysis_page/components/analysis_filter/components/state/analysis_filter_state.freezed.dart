f7f6369ca0ff076a171b0c859da3334e3fe5f316
mumumu3560 <puzmgamgam121@gmail.com>
2024-07-25 19:01:04 +0900

    フィルターの状態をクラスにしている。 freezedを使っている。

---
+++ lib/features/pages/analysis_page/components/analysis_filter/components/state/analysis_filter_state.freezed.dart
@@ -0,0 +1,300 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'analysis_filter_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$AnalysisFilterState {
+  List<DriftTradeTag> get allTags => throw _privateConstructorUsedError;
+  List<DriftTradeTag> get filteredTags => throw _privateConstructorUsedError;
+  List<String> get genres => throw _privateConstructorUsedError;
+  String? get selectedGenre => throw _privateConstructorUsedError;
+  List<String> get selectedTags => throw _privateConstructorUsedError;
+  bool get isBuy => throw _privateConstructorUsedError;
+  bool get useAndForTags => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $AnalysisFilterStateCopyWith<AnalysisFilterState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $AnalysisFilterStateCopyWith<$Res> {
+  factory $AnalysisFilterStateCopyWith(
+          AnalysisFilterState value, $Res Function(AnalysisFilterState) then) =
+      _$AnalysisFilterStateCopyWithImpl<$Res, AnalysisFilterState>;
+  @useResult
+  $Res call(
+      {List<DriftTradeTag> allTags,
+      List<DriftTradeTag> filteredTags,
+      List<String> genres,
+      String? selectedGenre,
+      List<String> selectedTags,
+      bool isBuy,
+      bool useAndForTags});
+}
+
+/// @nodoc
+class _$AnalysisFilterStateCopyWithImpl<$Res, $Val extends AnalysisFilterState>
+    implements $AnalysisFilterStateCopyWith<$Res> {
+  _$AnalysisFilterStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? allTags = null,
+    Object? filteredTags = null,
+    Object? genres = null,
+    Object? selectedGenre = freezed,
+    Object? selectedTags = null,
+    Object? isBuy = null,
+    Object? useAndForTags = null,
+  }) {
+    return _then(_value.copyWith(
+      allTags: null == allTags
+          ? _value.allTags
+          : allTags // ignore: cast_nullable_to_non_nullable
+              as List<DriftTradeTag>,
+      filteredTags: null == filteredTags
+          ? _value.filteredTags
+          : filteredTags // ignore: cast_nullable_to_non_nullable
+              as List<DriftTradeTag>,
+      genres: null == genres
+          ? _value.genres
+          : genres // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      selectedGenre: freezed == selectedGenre
+          ? _value.selectedGenre
+          : selectedGenre // ignore: cast_nullable_to_non_nullable
+              as String?,
+      selectedTags: null == selectedTags
+          ? _value.selectedTags
+          : selectedTags // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      isBuy: null == isBuy
+          ? _value.isBuy
+          : isBuy // ignore: cast_nullable_to_non_nullable
+              as bool,
+      useAndForTags: null == useAndForTags
+          ? _value.useAndForTags
+          : useAndForTags // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$AnalysisFilterStateImplCopyWith<$Res>
+    implements $AnalysisFilterStateCopyWith<$Res> {
+  factory _$$AnalysisFilterStateImplCopyWith(_$AnalysisFilterStateImpl value,
+          $Res Function(_$AnalysisFilterStateImpl) then) =
+      __$$AnalysisFilterStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {List<DriftTradeTag> allTags,
+      List<DriftTradeTag> filteredTags,
+      List<String> genres,
+      String? selectedGenre,
+      List<String> selectedTags,
+      bool isBuy,
+      bool useAndForTags});
+}
+
+/// @nodoc
+class __$$AnalysisFilterStateImplCopyWithImpl<$Res>
+    extends _$AnalysisFilterStateCopyWithImpl<$Res, _$AnalysisFilterStateImpl>
+    implements _$$AnalysisFilterStateImplCopyWith<$Res> {
+  __$$AnalysisFilterStateImplCopyWithImpl(_$AnalysisFilterStateImpl _value,
+      $Res Function(_$AnalysisFilterStateImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? allTags = null,
+    Object? filteredTags = null,
+    Object? genres = null,
+    Object? selectedGenre = freezed,
+    Object? selectedTags = null,
+    Object? isBuy = null,
+    Object? useAndForTags = null,
+  }) {
+    return _then(_$AnalysisFilterStateImpl(
+      allTags: null == allTags
+          ? _value._allTags
+          : allTags // ignore: cast_nullable_to_non_nullable
+              as List<DriftTradeTag>,
+      filteredTags: null == filteredTags
+          ? _value._filteredTags
+          : filteredTags // ignore: cast_nullable_to_non_nullable
+              as List<DriftTradeTag>,
+      genres: null == genres
+          ? _value._genres
+          : genres // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      selectedGenre: freezed == selectedGenre
+          ? _value.selectedGenre
+          : selectedGenre // ignore: cast_nullable_to_non_nullable
+              as String?,
+      selectedTags: null == selectedTags
+          ? _value._selectedTags
+          : selectedTags // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      isBuy: null == isBuy
+          ? _value.isBuy
+          : isBuy // ignore: cast_nullable_to_non_nullable
+              as bool,
+      useAndForTags: null == useAndForTags
+          ? _value.useAndForTags
+          : useAndForTags // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$AnalysisFilterStateImpl implements _AnalysisFilterState {
+  _$AnalysisFilterStateImpl(
+      {required final List<DriftTradeTag> allTags,
+      required final List<DriftTradeTag> filteredTags,
+      required final List<String> genres,
+      this.selectedGenre,
+      final List<String> selectedTags = const [],
+      this.isBuy = true,
+      this.useAndForTags = false})
+      : _allTags = allTags,
+        _filteredTags = filteredTags,
+        _genres = genres,
+        _selectedTags = selectedTags;
+
+  final List<DriftTradeTag> _allTags;
+  @override
+  List<DriftTradeTag> get allTags {
+    if (_allTags is EqualUnmodifiableListView) return _allTags;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_allTags);
+  }
+
+  final List<DriftTradeTag> _filteredTags;
+  @override
+  List<DriftTradeTag> get filteredTags {
+    if (_filteredTags is EqualUnmodifiableListView) return _filteredTags;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_filteredTags);
+  }
+
+  final List<String> _genres;
+  @override
+  List<String> get genres {
+    if (_genres is EqualUnmodifiableListView) return _genres;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_genres);
+  }
+
+  @override
+  final String? selectedGenre;
+  final List<String> _selectedTags;
+  @override
+  @JsonKey()
+  List<String> get selectedTags {
+    if (_selectedTags is EqualUnmodifiableListView) return _selectedTags;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_selectedTags);
+  }
+
+  @override
+  @JsonKey()
+  final bool isBuy;
+  @override
+  @JsonKey()
+  final bool useAndForTags;
+
+  @override
+  String toString() {
+    return 'AnalysisFilterState(allTags: $allTags, filteredTags: $filteredTags, genres: $genres, selectedGenre: $selectedGenre, selectedTags: $selectedTags, isBuy: $isBuy, useAndForTags: $useAndForTags)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$AnalysisFilterStateImpl &&
+            const DeepCollectionEquality().equals(other._allTags, _allTags) &&
+            const DeepCollectionEquality()
+                .equals(other._filteredTags, _filteredTags) &&
+            const DeepCollectionEquality().equals(other._genres, _genres) &&
+            (identical(other.selectedGenre, selectedGenre) ||
+                other.selectedGenre == selectedGenre) &&
+            const DeepCollectionEquality()
+                .equals(other._selectedTags, _selectedTags) &&
+            (identical(other.isBuy, isBuy) || other.isBuy == isBuy) &&
+            (identical(other.useAndForTags, useAndForTags) ||
+                other.useAndForTags == useAndForTags));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(_allTags),
+      const DeepCollectionEquality().hash(_filteredTags),
+      const DeepCollectionEquality().hash(_genres),
+      selectedGenre,
+      const DeepCollectionEquality().hash(_selectedTags),
+      isBuy,
+      useAndForTags);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$AnalysisFilterStateImplCopyWith<_$AnalysisFilterStateImpl> get copyWith =>
+      __$$AnalysisFilterStateImplCopyWithImpl<_$AnalysisFilterStateImpl>(
+          this, _$identity);
+}
+
+abstract class _AnalysisFilterState implements AnalysisFilterState {
+  factory _AnalysisFilterState(
+      {required final List<DriftTradeTag> allTags,
+      required final List<DriftTradeTag> filteredTags,
+      required final List<String> genres,
+      final String? selectedGenre,
+      final List<String> selectedTags,
+      final bool isBuy,
+      final bool useAndForTags}) = _$AnalysisFilterStateImpl;
+
+  @override
+  List<DriftTradeTag> get allTags;
+  @override
+  List<DriftTradeTag> get filteredTags;
+  @override
+  List<String> get genres;
+  @override
+  String? get selectedGenre;
+  @override
+  List<String> get selectedTags;
+  @override
+  bool get isBuy;
+  @override
+  bool get useAndForTags;
+  @override
+  @JsonKey(ignore: true)
+  _$$AnalysisFilterStateImplCopyWith<_$AnalysisFilterStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
